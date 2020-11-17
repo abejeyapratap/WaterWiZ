3daccfbd6ed84fb3ce5564d1d12a75d58f547981
Eric Rabil <ericjrabil@gmail.com>
2020-11-17 16:25:46 -0500

    Initial implementation of suppliers pages

--- js/suppliers.js
+++ js/suppliers.js
@@ -1,4 +1,15 @@
 "use strict";
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
 var SuppliersCore;
 (function (SuppliersCore) {
     SuppliersCore.suppliers = {};
@@ -41,12 +52,182 @@ var SuppliersCore;
     }
     SuppliersCore.getCounty = getCounty;
 })(SuppliersCore || (SuppliersCore = {}));
+var SuppliersDOM;
+(function (SuppliersDOM) {
+    function element(tagName, children, attributes) {
+        if (children === void 0) { children = []; }
+        if (attributes === void 0) { attributes = {}; }
+        var element = document.createElement(tagName);
+        if (!Array.isArray(children))
+            children = [children];
+        for (var _i = 0, children_1 = children; _i < children_1.length; _i++) {
+            var child = children_1[_i];
+            if (typeof child === "string")
+                element.appendChild(document.createTextNode(child));
+            else
+                element.appendChild(child);
+        }
+        for (var key in attributes) {
+            element.setAttribute(key, attributes[key]);
+        }
+        return element;
+    }
+    SuppliersDOM.element = element;
+    function makeHeading(_a) {
+        var name = _a.name, id = _a.id;
+        return element("div", [
+            element("h1", name, { id: id }),
+            element("hr")
+        ], {
+            id: "supplier-heading"
+        });
+    }
+    SuppliersDOM.makeHeading = makeHeading;
+    function makeSystem(_a) {
+        var name = _a.name, waterStatus = _a.waterStatus, description = _a.description, stats = _a.stats;
+        return element("div", [
+            element("h3", name, { class: "system-name" }),
+            element("p", description, { class: "system-description" }),
+            element("p", [
+                element("strong", "Water Status: "),
+                waterStatus
+            ], { class: "water-status" }),
+            element("div", [
+                element("div", [], __assign({ name: "chart" }, stats.reduce(function (collector, stat, index) { return (collector["stat-" + index] = stat.toString(), collector); }, {})))
+            ])
+        ], {
+            class: "system-container"
+        });
+    }
+    SuppliersDOM.makeSystem = makeSystem;
+    function makeSystems(systems) {
+        return element("div", systems.map(function (system) { return makeSystem(system); }), {
+            class: "systems-container"
+        });
+    }
+    SuppliersDOM.makeSystems = makeSystems;
+    function makeCounty(_a) {
+        var id = _a.id, name = _a.name, systems = _a.systems;
+        return element("div", [
+            element("h2", name, { id: id }),
+            makeSystems(systems)
+        ], {
+            class: "county-container"
+        });
+    }
+    SuppliersDOM.makeCounty = makeCounty;
+    function makeCounties(counties) {
+        return element("div", counties.map(function (c) { return makeCounty(c); }), {
+            class: "counties-container"
+        });
+    }
+    SuppliersDOM.makeCounties = makeCounties;
+})(SuppliersDOM || (SuppliersDOM = {}));
+var SuppliersCharting;
+(function (SuppliersCharting) {
+    var element = SuppliersDOM.element;
+    var definitions = [];
+    function loadStatisticDefinitions(cb) {
+        var chartsLoaded = false;
+        var statsLoaded = false;
+        google.charts.load("current", { packages: ["bar"] });
+        google.charts.setOnLoadCallback(function () {
+            if (statsLoaded && cb)
+                cb();
+            chartsLoaded = true;
+        });
+        $.getJSON("/api/stats-definitions.json", function (data) {
+            definitions = data.stats;
+            if (chartsLoaded && cb)
+                cb();
+            statsLoaded = true;
+        });
+    }
+    SuppliersCharting.loadStatisticDefinitions = loadStatisticDefinitions;
+    function setupChart(chart) {
+        var stats = [];
+        var charts = [];
+        for (var attributeIndex in chart.attributes) {
+            var attribute = chart.attributes[attributeIndex];
+            var attributeName = attribute.name;
+            if (!attributeName)
+                continue;
+            if (attributeName.slice(0, 5) !== "stat-")
+                continue;
+            var index = +attributeName.split("-")[1];
+            if (isNaN(index))
+                continue;
+            var value = +attribute.value;
+            if (isNaN(value))
+                continue;
+            stats[index] = value;
+        }
+        stats.forEach(function (stat, index) {
+            var definition = definitions[index];
+            if (!definition)
+                return;
+            var data = google.visualization.arrayToDataTable([
+                ["", "Normal", "Actual"],
+                [" ", definition.safeRange[1], stat]
+            ]);
+            var chartContainer = document.createElement("div");
+            var container = element("div", [
+                element("span", definition.name, { class: "stat-name" }),
+                chartContainer
+            ], { class: "stat-container" });
+            chart.appendChild(container);
+            var gChart = new google.charts.Bar(chartContainer);
+            charts.push([gChart, [data, {
+                        bars: "horizontal",
+                        height: 50,
+                        legend: "none",
+                        tooltip: {
+                            textStyle: {
+                                fontSize: 12
+                            }
+                        },
+                        allowAsync: true
+                    }]]);
+        });
+        return charts;
+    }
+    SuppliersCharting.setupChart = setupChart;
+})(SuppliersCharting || (SuppliersCharting = {}));
 var SupplierPage = /** @class */ (function () {
     function SupplierPage() {
+        this.storage = {};
     }
     SupplierPage.prototype.init = function () {
-        console.log(this.supplier);
+        if (!this.supplier)
+            return;
+        this.main.appendChild(SuppliersDOM.makeHeading(this.supplier));
+        this.main.appendChild(SuppliersDOM.makeCounties(this.supplier.counties));
+        var charts = this.main.querySelectorAll("[name=chart]");
+        var chartPartials = Array.prototype.map.call(charts, function (chart) { return SuppliersCharting.setupChart(chart); });
+        var _loop_1 = function (someCharts) {
+            requestAnimationFrame(function () {
+                for (var _i = 0, someCharts_1 = someCharts; _i < someCharts_1.length; _i++) {
+                    var _a = someCharts_1[_i], chart = _a[0], _b = _a[1], opt1 = _b[0], opt2 = _b[1];
+                    chart.draw(opt1, opt2);
+                }
+            });
+        };
+        for (var _i = 0, chartPartials_1 = chartPartials; _i < chartPartials_1.length; _i++) {
+            var someCharts = chartPartials_1[_i];
+            _loop_1(someCharts);
+        }
+        console.log("told them to do it");
     };
+    SupplierPage.prototype.element = function (selector) {
+        return document.querySelector(selector);
+    };
+    Object.defineProperty(SupplierPage.prototype, "main", {
+        get: function () {
+            return this.storage.main || (this.storage.main = this.element("main#main-content"));
+        },
+        enumerable: false,
+        configurable: true
+    });
     Object.defineProperty(SupplierPage.prototype, "supplier", {
         get: function () {
             return SuppliersCore.getSupplier();
@@ -64,8 +245,19 @@ var SupplierPage = /** @class */ (function () {
     return SupplierPage;
 }());
 $(function () {
-    SuppliersCore.loadSuppliers(function () {
+    function completed() {
         var page = new SupplierPage();
         page.init();
+    }
+    var loadedDefs = false, loadedSuppliers = false;
+    SuppliersCharting.loadStatisticDefinitions(function () {
+        if (loadedSuppliers)
+            completed();
+        loadedDefs = true;
+    });
+    SuppliersCore.loadSuppliers(function () {
+        if (loadedDefs)
+            completed();
+        loadedSuppliers = true;
     });
 });
--- ts/suppliers.ts
+++ ts/suppliers.ts
@@ -66,24 +66,225 @@ namespace SuppliersCore {
     }
 }
 
-class SupplierPage {
-    init() {
-        console.log(this.supplier);
+namespace SuppliersDOM {
+    type Child = HTMLElement | string;
+
+    export function element(tagName: Parameters<typeof document["createElement"]>[0], children: Child | Child[] = [], attributes: Record<string, string> = {}): HTMLElement {
+        const element = document.createElement(tagName);
+
+        if (!Array.isArray(children)) children = [children];
+
+        for (let child of children) {
+            if (typeof child === "string") element.appendChild(document.createTextNode(child));
+            else element.appendChild(child);
+        }
+
+        for (let key in attributes) {
+            element.setAttribute(key, attributes[key]);
+        }
+
+        return element;
     }
 
-    get supplier() {
+    export function makeHeading({ name, id }: Supplier): HTMLElement {
+        return element("div", [
+            element("h1", name, { id }),
+            element("hr")
+        ], {
+            id: "supplier-heading"
+        });
+    }
+
+    export function makeSystem({ name, waterStatus, description, stats }: WaterSystem): HTMLElement {
+        return element("div", [
+            element("h3", name, { class: "system-name" }),
+            element("p", description, { class: "system-description" }),
+            element("p", [
+                element("strong", "Water Status: "),
+                waterStatus
+            ], { class: "water-status" }),
+            element("div", [
+                element("div", [], {
+                    name: "chart",
+                    ...stats.reduce((collector, stat, index) => (collector["stat-" + index] = stat.toString(), collector), {} as Record<string, string>)
+                })
+            ])
+        ], {
+            class: "system-container"
+        });
+    }
+
+    export function makeSystems(systems: WaterSystem[]): HTMLElement {
+        return element("div", systems.map(system => makeSystem(system)), {
+            class: "systems-container"
+        });
+    }
+
+    export function makeCounty({ id, name, systems }: County): HTMLElement {
+        return element("div", [
+            element("h2", name, { id }),
+            makeSystems(systems)
+        ], {
+            class: "county-container"
+        });
+    }
+
+    export function makeCounties(counties: County[]): HTMLElement {
+        return element("div", counties.map(c => makeCounty(c)), {
+            class: "counties-container"
+        });
+    }
+}
+
+namespace SuppliersCharting {
+    const { element } = SuppliersDOM;
+
+    export interface StatisticDefinition {
+        name: string;
+        range: [number, number];
+        safeRange: [number, number];
+        unit: string;
+        color?: string;
+    }
+
+    export interface StatisticDefinitionRoot {
+        stats: StatisticDefinition[];
+    }
+
+    let definitions: StatisticDefinition[] = [];
+
+    export function loadStatisticDefinitions(cb?: () => void) {
+        let chartsLoaded = false;
+        let statsLoaded = false;
+
+        google.charts.load("current", { packages: ["bar"] });
+        google.charts.setOnLoadCallback(function() {
+            if (statsLoaded && cb) cb();
+            chartsLoaded = true;
+        })
+
+        $.getJSON("/api/stats-definitions.json", (data: StatisticDefinitionRoot) => {
+            definitions = data.stats;
+            
+            if (chartsLoaded && cb) cb();
+            statsLoaded = true;
+        });
+    }
+
+    export function setupChart(chart: HTMLElement): [any, any[]][] {
+        const stats: number[] = [];
+        const charts: [any, any[]][] = [];
+
+        for (let attributeIndex in chart.attributes) {
+            const attribute = chart.attributes[attributeIndex];
+
+            const attributeName = attribute.name;
+            if (!attributeName) continue;
+
+            if (attributeName.slice(0, 5) !== "stat-") continue;
+            const index = +attributeName.split("-")[1];
+            if (isNaN(index)) continue;
+
+            const value = +attribute.value;
+            if (isNaN(value)) continue;
+
+            stats[index] = value;
+        }
+
+        stats.forEach((stat, index) => {
+            const definition = definitions[index];
+            if (!definition) return;
+
+            const data = google.visualization.arrayToDataTable([
+                ["", "Normal", "Actual"],
+                [" ", definition.safeRange[1], stat]
+            ]);
+
+            const chartContainer = document.createElement("div");
+
+            const container = element("div", [
+                element("span", definition.name, { class: "stat-name" }),
+                chartContainer
+            ], { class: "stat-container" });
+
+            chart.appendChild(container);
+
+            const gChart = new (google.charts as any).Bar(chartContainer);
+            
+            charts.push([gChart, [data, {
+                bars: "horizontal",
+                height: 50,
+                legend: "none",
+                tooltip: {
+                    textStyle: {
+                        fontSize: 12
+                    }
+                },
+                allowAsync: true
+            }]]);
+        });
+
+        return charts;
+    }
+}
+
+class SupplierPage {
+    storage: Record<string, HTMLElement> = {};
+
+    init() {
+        if (!this.supplier) return;
+        
+        this.main.appendChild(SuppliersDOM.makeHeading(this.supplier));
+        this.main.appendChild(SuppliersDOM.makeCounties(this.supplier.counties));
+
+        const charts = this.main.querySelectorAll("[name=chart]");
+
+        const chartPartials = Array.prototype.map.call(charts, chart => SuppliersCharting.setupChart(chart as HTMLElement)) as [any, any[]][];
+
+        for (let someCharts of chartPartials) {
+            requestAnimationFrame(function() {
+                for (let [chart, [opt1, opt2]] of someCharts) {
+                    chart.draw(opt1, opt2);
+                }
+            });
+        }
+
+        console.log("told them to do it");
+    }
+
+    private element(selector: string): HTMLElement {
+        return document.querySelector(selector) as HTMLElement;
+    }
+
+    get main(): HTMLElement {
+        return this.storage.main || (this.storage.main = this.element("main#main-content"));
+    }
+
+    get supplier(): Supplier | null {
         return SuppliersCore.getSupplier();
     }
 
-    get county() {
+    get county(): County | null {
         return SuppliersCore.getCounty();
     }
 }
 
 $(function() {
-    SuppliersCore.loadSuppliers(() => {
+    function completed() {
         const page = new SupplierPage();
-
+    
         page.init();
+    }
+
+    let loadedDefs = false, loadedSuppliers = false;
+
+    SuppliersCharting.loadStatisticDefinitions(() => {
+        if (loadedSuppliers) completed();
+        loadedDefs = true;
+    });
+
+    SuppliersCore.loadSuppliers(() => {
+        if (loadedDefs) completed();
+        loadedSuppliers = true;
     });
 });
