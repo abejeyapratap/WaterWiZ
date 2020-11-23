3cae4a26e3b0e5fc26a37b81a8179f8713ba9c4f
Eric Rabil <ericjrabil@gmail.com>
2020-11-22 21:59:23 -0500

    disable pause

--- js/home.js
+++ js/home.js
@@ -2,6 +2,8 @@ $(function() {
     $('.carousel').slick({
         autoplay: true,
         autoplaySpeed: 5000,
+        pauseOnFocus: false,
+        pauseOnHover: false,
         slidesToScroll: 1
     });
 });
